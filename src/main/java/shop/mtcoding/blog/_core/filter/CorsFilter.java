0283b48688befdc8369c8a9e01d533af97fc4725
sxias <a32176740@gmail.com>
2025-05-16 10:24:06 +0900

    swagger 작설정 완료

--- src/main/java/shop/mtcoding/blog/_core/filter/CorsFilter.java
+++ src/main/java/shop/mtcoding/blog/_core/filter/CorsFilter.java
@@ -19,7 +19,7 @@ public class CorsFilter implements Filter {
         log.debug("Origin : " + origin);
 
         response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
-//        response.setHeader("Access-Control-Expose-Headers", "Authorization"); // 응답 해줄 수 있는 헤더
+//        response.setHeader("Access-Control-Expose-Headers", "Authorization"); // JS로 접근할 수 있도록 허용할 헤더
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE, OPTIONS"); // OPTIONS : Preflight
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Key, Content-Type, Accept, Authorization"); // 요청 받을 수 있는 헤더
